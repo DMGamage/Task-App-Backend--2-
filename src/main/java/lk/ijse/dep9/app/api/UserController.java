2f3248a6a3c0b72a2c2ed92a7aa9623fcabaff3d
DMGamage <gamagemadhushan@gmail.com>
2023-01-11 02:04:48 +0530

    Add error response msg DTO

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -1,6 +1,18 @@
 package lk.ijse.dep9.app.api;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep9.app.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@CrossOrigin
 @RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes="application/json")
+    public void createUserAccount(@Valid @RequestBody UserDTO user){
+        System.out.println(user);
+
+    }
 }
--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -3,7 +3,11 @@ package lk.ijse.dep9.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
@@ -12,8 +16,13 @@ import java.io.Serializable;
 
 public class UserDTO implements Serializable {
 
+  @NotBlank
+  @Pattern(regexp = "^[A-Za-z ]+$")
     private String fullName;
+  @NotBlank
     private String username;
+    @NotEmpty
+    @Length(min=3)
     private String password;
 
 }
